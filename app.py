d2c233153b33df5af5e3ae4223c0bef1860bab18
dev <dev@example.com>
2022-06-13 11:41:48 +0300

    update files

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from pymongo import MongoClient
 now = datetime.now()
 
 app = Flask(__name__)
-CONNECTION_STRING = "mongodb://root:itay@mongo:27017/"
+CONNECTION_STRING = "mongodb://root:itay@mongo-mongodb-0.mongo-mongodb-headless:27017/"
 
 def get_database():
 
@@ -41,7 +41,7 @@ def get_database():
 def health():
     try:
         conn = MongoClient(CONNECTION_STRING)
-        return render_template("health.html",status ="You habe been connected Successfully!"),200
+        return render_template("health.html",status ="You have been connected Successfully!"),200
     except:
         return render_template("health.html",status ="Oops.. The database is unreachable :("),500
 
