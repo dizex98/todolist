9c8f7bec1d697d4b265231622ffae33716b2f8e7
dev <dev@example.com>
2022-07-04 14:11:11 +0300

    updated argocd config

--- terraform/helm_realease.tf
+++ terraform/helm_realease.tf
@@ -29,34 +29,34 @@ resource "helm_release" "mongodb" {
 }
 
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "argocd"
-  version    = "4.9.8"
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "4.9.8"
   create_namespace = true
 }
 
 resource "kubectl_manifest" "config" {
-  yaml_body = file("../argocd/application.yaml")
+  yaml_body  = file("../argocd/application.yaml")
   depends_on = [helm_release.argocd]
 }
 
 resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  namespace  = "prometheus"
-  version    = "36.2.1"
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "prometheus"
+  version          = "36.2.1"
   create_namespace = true
 }
 
 resource "helm_release" "elastic" {
-  name       = "elastic"
-  repository = "https://helm.elastic.co"
-  chart      = "eck-operator"
-  namespace  = "elastic"
-  version    = "2.3.0"
+  name             = "elastic"
+  repository       = "https://helm.elastic.co"
+  chart            = "eck-operator"
+  namespace        = "elastic"
+  version          = "2.3.0"
   create_namespace = true
 }
 
