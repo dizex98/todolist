a4de02f9588eb54eedbb09ac9e1411229c9df094
dev <dev@example.com>
2022-07-14 12:20:42 +0300

    prepearing v.3

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def get_database():
 def health():
     try:
         conn = MongoClient(CONNECTION_STRING)
-        return render_template("health.html",status ="You have been connected Successfully!!"),200
+        return render_template("health.html",status ="You have been connected Successfully!!!"),200
     except:
         return render_template("health.html",status ="Oops.. The database is unreachable :("),500
 
